4625e123daf59c7a289967e753ad1b616acedc1a
shree <shree@LAPTOP-2QEA4LIA>
2024-01-12 10:46:37 +0530

    By.saurabh

---
+++ Git_Demo3/src/com/cjc/demo/B.java
@@ -0,0 +1,23 @@
+package com.cjc.demo;
+
+public class B {
+	
+	
+	
+	public static void m1()
+	{
+		System.out.println("THIS IS M1");
+	}
+	
+	
+	
+	
+	
+public static void main(String[] args) 
+{
+	B.m1();
+	
+}
+	
+
+}
