ce267d8f45399a404ed8a76a4efdabe59b5b57cf
saurabh13061998 <143089565+saurabh13061998@users.noreply.github.com>
2024-01-12 10:52:21 +0530

    Update B.java

--- Git_Demo3/src/com/cjc/demo/B.java
+++ Git_Demo3/src/com/cjc/demo/B.java
@@ -9,14 +9,17 @@ public class B {
 		System.out.println("THIS IS M1");
 	}
 	
-	
-	
+
+	public static void m2()
+	{
+             System.out.println("THIS IS M2");
+	}
 	
 	
 public static void main(String[] args) 
 {
 	B.m1();
-	
+	B.m2();
 }
 	
 
